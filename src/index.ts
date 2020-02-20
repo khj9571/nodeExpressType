503123788f1005b58da142e29d9d98ed074adf91
김회주 <dnflskfk7!>
2020-02-20 15:43:12 +0900

    수정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express from 'express';
 const app = express();
 
+/** 전체 인터셉터 */
+app.use(function(req, res, next) {
+  //인터셉터 역할 부여
+  console.log('call global interFace')   
+  next();
+});
+
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World!');
 });
@@ -10,3 +18,15 @@ app.listen(3000, () => {
 });
 
 app.use('/user', require('./routers/user'))
+
+
+
+/** 부분 인터셉터 
+ const router = express.Router();
+router.all('/매핑주소',function(req, res, next) {
+    //인터셉터
+    next();
+}, function(req, res, next) {
+  //인터셉터 이후 행동
+}); 
+*/
--- src/routers/user.ts
+++ src/routers/user.ts
@@ -4,11 +4,8 @@ const router = Router()
 
 import user from '../controllers/user'
 //import * as user from '../controllers/user'
-// router.get('/echo', echo.echo)
+//router.get('/echo', echo.echo)
 
 router.get('/getUser/:id', user.getUser)
-
-
-
 
 module.exports = router
